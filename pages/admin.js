2fb3a5b4904e8384d2a7386b5431b615ae5aca6a
telecustom <telecustom@gmail.com>
2025-08-25 11:14:54 -0400

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,8 +1,6 @@
 import { supabase } from '../lib/supabaseClient'
 
 export default function Admin() {
-  return <h1>Admin Dashboard</h1>;
-}
   async function applyMultiplier(team, multiplier) {
     const { data, error } = await supabase
       .from('picks')
